6f94cd16fb8f4d053940240b31f77edfc058dbba
michelcub <orestelopez96@gmail.com>
2023-05-30 12:43:57 +0100

    fix root img

--- src/Components/CardQR.jsx
+++ src/Components/CardQR.jsx
@@ -3,7 +3,7 @@ import './CardQR.css'
 export const CardQR = () => {
     return(
         <article className='content_card_qr'>
-            <img className="img_card_qr" src="../../public/images/image-qr-code.png" alt="" />
+            <img className="img_card_qr" src="../assets/image-qr-code.png" alt="" />
             <div className="body_card_qr">
                 <h3 className="title_card_qr">Improve your Front-end skills bu building proyects</h3>
                 <p className="paragraph_card_qr">Scan the QR code to visit Frontend Mentor andtake your coding skils to the next level</p>
